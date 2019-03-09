abef0adb4845e0f367a4995525371f36abcc8196
James Smith <jesmith026@outlook.com>
2019-03-09 15:30:49 -0600

    add param for specifying dll location

--- DacDeploy/task.ps1
+++ DacDeploy/task.ps1
@@ -8,25 +8,56 @@ $server = Get-VstsInput -Name 'server' -Require;
 $username = Get-VstsInput -Name 'username' -Require;
 $password = Get-VstsInput -Name 'password' -Require;
 $blockOnDataLoss = Get-VstsInput -Name 'blockOnDataLoss' -Require;
+$dacdll = Get-VstsInput -Name "dacdll";
 
+if ([string]::IsNullOrEmpty($dacdll)) {
+    $dacdll = "C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\Microsoft.SqlServer.Dac.dll";
+}
 
 Write-Host "DACPAC File: $dacpac";
 Write-Host "Server: $server";
 Write-Host "Username: $username";
 
-$dbName = (Get-ChildItem $dacpac).BaseName;
+. {
+    trap { Write-Host "Error loading database project: $dacpac"; Break; }
+    
+    Write-Host "Loading database project";
+    $dbName = (Get-ChildItem $dacpac).BaseName;
+}
 
 Write-Host "Database Name: $dbName";
 
-add-type -path "C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin\Microsoft.SqlServer.Dac.dll";
+. {
+    trap { Write-Host "Error loading dac dll"; Break; }
 
-$dacService = new-object Microsoft.SqlServer.Dac.DacServices "Data Source=$server;User Id=$username;Password=$password;";
+    Write-Host "Loading dac dll"
+    add-type -path $dacdll;
+}
 
-$deployOptions = New-Object Microsoft.SqlServer.Dac.DacDeployOptions;
-$deployOptions.RegisterDataTierApplication = $true;
-$deployOptions.BlockOnPossibleDataLoss = $blockOnDataLoss;
-$deployOptions.BlockWhenDriftDetected = $false;
+. {
+    trap { Write-Host "Error creating dac service"; Break; }
 
-$dp = [Microsoft.SqlServer.Dac.DacPackage]::Load($dacpac);
+    Write-Host "Creating dac service"
+    $dacService = new-object Microsoft.SqlServer.Dac.DacServices "Data Source=$server;User Id=$username;Password=$password;";
+}
 
-$dacService.deploy($dp, $dbName, "True", $deployOptions);
+. {
+    trap { Write-Host "Error configuring deployment options"; Break; }
+
+    Write-Host "Configuring deployment options...";
+
+    $deployOptions = New-Object Microsoft.SqlServer.Dac.DacDeployOptions;
+    $deployOptions.RegisterDataTierApplication = $true;
+    $deployOptions.BlockOnPossibleDataLoss = $blockOnDataLoss;
+    $deployOptions.BlockWhenDriftDetected = $false;
+}
+
+. {
+    trap { Write-Host "Error deploying changes for server: ($server) and database: ($dbName)"}
+
+    Write-Host "Deploying database changes for server: ($server) and database: ($dbName)...";
+    $dp = [Microsoft.SqlServer.Dac.DacPackage]::Load($dacpac);
+    $dacService.deploy($dp, $dbName, "True", $deployOptions);
+}
+
+Write-Host "Deployment complete"
