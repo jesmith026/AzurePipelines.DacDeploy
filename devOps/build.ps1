0d86be7128f40f0b97e674506b38ee9d1525cf8c
James Smith <jesmith026@outlook.com>
2019-03-10 21:51:27 -0500

    update build script and set to v1.

--- devOps/build.ps1
+++ devOps/build.ps1
@@ -1,1 +1,17 @@
-tfx extension create --rev-version --output-path $PSScriptRoot\..\artifacts\ --manifest-globs $PSScriptRoot\..\vss-extension.json
+param(
+    [switch] $IncrementVersion
+)
+
+
+function checkAddRevVersion([parameter(ValueFromPipeline)]$cmd) {
+    if ($PSBoundParameters.ContainsKey('IncrementVersion')) {
+        $cmd += ' --rev-version';
+    }
+    $cmd;
+}
+
+
+$cmd = 'tfx extension create' | checkAddRevVersion;
+$cmd += " --output-path $PSScriptRoot\..\artifacts\ --manifest-globs $PSScriptRoot\..\vss-extension.json";
+
+Invoke-Expression $cmd;
